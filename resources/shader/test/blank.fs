620526bb99723afa3ea9ba6b6468e312d45a58c3
akin <akin@localhost>
2012-02-08 01:03:14 +0200

    fine..

--- resources/shader/test/blank.fs
+++ resources/shader/test/blank.fs
@@ -3,9 +3,15 @@
 
 out vec4 color;
  
+in vec3 vertex;
+
 void main(void)
 {
-    vec4 color = vec4( 0.2 , 0.5 , 1.0 , 1.0 );
+	vec3 normalizedColor = (vertex + 1.0) / 2.0;
+	
+    //vec4 color = vec4( smoothstep( 1.0 , 0.0 , normalizedColor ) , 1.0 );
+    
+    vec4 color = vec4( normalizedColor , 1.0 );
     
     //Blend the original color with the averaged pixels
     gl_FragColor = color;
--- resources/shader/test/blank.vs
+++ resources/shader/test/blank.vs
@@ -4,7 +4,10 @@
 uniform mat4 model;
 uniform mat4 lense;
 
+out vec3 vertex;
+
 void main(void)
 {
    gl_Position = lense * model * gl_Vertex;
+   vertex = gl_Vertex.xyz;
 }
