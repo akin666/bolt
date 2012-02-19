76d297a259900d5730caac066052fe9bea1767db
akin <akin@localhost>
2012-02-19 23:35:39 +0200

    khlj

--- resources/shader/test/blank.fs
+++ resources/shader/test/blank.fs
@@ -9,9 +9,9 @@ void main(void)
 {
 	color = vec4( 0.0 );
     
-	if( mod( vertex.x , 16.0 ) > 15.0 || mod( vertex.z , 16.0 ) > 15.0 ) 
+	if( mod( vertex.x , 16.0 ) > 15.5 || mod( vertex.z , 16.0 ) > 15.5 ) 
 	{
-		color = vec4( 1.0 );
-//		color = vec4( 1.0 , 1.0 , 1.0 , 1.0 );
+//		color = vec4(.20, .40, .90, 1.0);
+		color = vec4( 0.8 , 0.8 , 0.2 , 1.0 );
 	}
 }
